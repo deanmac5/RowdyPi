43a8c21e5eef56b55becb70956d8896068c122a4
Dean Macaulay <dean@magpies.net>
2016-01-05 11:54:32 +1100

    Added 2 more sensors

--- Humidity.py
+++ Humidity.py
@@ -6,11 +6,15 @@ sensor = Adafruit_DHT.DHT22
 
 kitchenPin = 22
 loungePin = 24
+supplyAirPin = 23
+outsideAirPin = 10
 
 
 
 khumidity, ktemp = Adafruit_DHT.read_retry(sensor, kitchenPin)
 lhumidity, ltemp = Adafruit_DHT.read_retry(sensor, loungePin)
+sahumidity, satemp = Adafruit_DHT.read_retry(sensor, supplyAirPin)
+oahumidity, oatemp = Adafruit_DHT.read_retry(sensor, outsideAirPin)
 
 if khumidity is not None and ktemp is not None:
     print 'Kitchen Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(ktemp, khumidity)
@@ -21,3 +25,13 @@ if lhumidity is not None and ltemp is not None:
     print 'Lounge Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(ltemp, lhumidity)
 else:
     print 'Failed to get Lounge reading. Try again!'
+
+if sahumidity is not None and satemp is not None:
+    print 'Supply Air Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(satemp, sahumidity)
+else:
+    print 'Failed to get Supply Air reading. Try again!'
+
+if oahumidity is not None and oatemp is not None:
+    print 'Outside Air Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(oatemp, oahumidity)
+else:
+    print 'Failed to get Outside Air reading. Try again!'
