55c9a8e953f1fbbee87279ea48f0b1976feacb06
Dean Macaulay <dean@magpies.net>
2016-01-05 15:51:25 +1100

    The start of the amalgamation

--- Humidity.py
+++ Humidity.py
@@ -1,61 +1,78 @@
-#!/usr/bin/python
 
 import Adafruit_DHT
 
 sensor = Adafruit_DHT.DHT22
 
-kitchenPin = 22
-loungePin = 24
-supplyAirPin = 23
-outsideAirPin = 10
-ExhaustAirPin = 18
-MBedroomPin = 17
+pins = {'Kitchen': 22, 'Lounge': 24, 'Supply Air': 23, 'Outside Air': 10, 'Exhaust Air': 18, 'Master Bedroom': 17}
+
+# kitchenPin = 22
+# loungePin = 24
+# supplyAirPin = 23
+# outsideAirPin = 10
+# ExhaustAirPin = 18
+# MBedroomPin = 17
 
 
-khumidity, ktemp = Adafruit_DHT.read_retry(sensor, kitchenPin)
-if khumidity is not None and ktemp is not None:
-    print 'Kitchen Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(ktemp, khumidity)
-else:
-    print 'Failed to get Kitchen reading. Try again!'
+def readDHT(pin):
+    hum, temp = Adafruit_DHT.read_retry(sensor, pin)
+    printOutput(hum,temp)
 
 
+def printOutput(hum,temp):
+    if hum is not None and temp is not None:
+        print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temp, hum)
+    else:
+        print 'Failed to get reading. Boo hiss'
 
-lhumidity, ltemp = Adafruit_DHT.read_retry(sensor, loungePin)
-if lhumidity is not None and ltemp is not None:
-    print 'Lounge Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(ltemp, lhumidity)
-else:
-    print 'Failed to get Lounge reading. Try again!'
-
-
-
-sahumidity, satemp = Adafruit_DHT.read_retry(sensor, supplyAirPin)
-if sahumidity is not None and satemp is not None:
-    print 'Supply Air Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(satemp, sahumidity)
-else:
-    print 'Failed to get Supply Air reading. Try again!'
+#For testing
+for room,pin in pins.items():
+    print 'Room %s is pin %s' % (room,pin)
+    readDHT(pin)
 
 
-
-oahumidity, oatemp = Adafruit_DHT.read_retry(sensor, outsideAirPin)
-if oahumidity is not None and oatemp is not None:
-    print 'Outside Air Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(oatemp, oahumidity)
-else:
-    print 'Failed to get Outside Air reading. Try again!'
-
-
-
-Eahumidity, Eatemp = Adafruit_DHT.read_retry(sensor, ExhaustAirPin)
-if Eahumidity is not None and Eatemp is not None:
-    print 'Exhaust Air Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(Eatemp, Eahumidity)
-else:
-    print 'Failed to get Exhaust Air reading. Try again!'
-
-
-
-
-MBrhumidity, MBrtemp = Adafruit_DHT.read_retry(sensor, MBedroomPin)
-
-if MBrhumidity is not None and MBrtemp is not None:
-    print 'Master Bedroom Air Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(MBrtemp, MBrhumidity)
-else:
-    print 'Failed to get Master Bedroom reading. Try again!'
+# if khumidity is not None and ktemp is not None:
+#     print 'Kitchen Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(ktemp, khumidity)
+# else:
+#     print 'Failed to get Kitchen reading. Try again!'
+#
+#
+#
+# lhumidity, ltemp = Adafruit_DHT.read_retry(sensor, loungePin)
+# if lhumidity is not None and ltemp is not None:
+#     print 'Lounge Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(ltemp, lhumidity)
+# else:
+#     print 'Failed to get Lounge reading. Try again!'
+#
+#
+#
+# sahumidity, satemp = Adafruit_DHT.read_retry(sensor, supplyAirPin)
+# if sahumidity is not None and satemp is not None:
+#     print 'Supply Air Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(satemp, sahumidity)
+# else:
+#     print 'Failed to get Supply Air reading. Try again!'
+#
+#
+#
+# oahumidity, oatemp = Adafruit_DHT.read_retry(sensor, outsideAirPin)
+# if oahumidity is not None and oatemp is not None:
+#     print 'Outside Air Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(oatemp, oahumidity)
+# else:
+#     print 'Failed to get Outside Air reading. Try again!'
+#
+#
+#
+# Eahumidity, Eatemp = Adafruit_DHT.read_retry(sensor, ExhaustAirPin)
+# if Eahumidity is not None and Eatemp is not None:
+#     print 'Exhaust Air Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(Eatemp, Eahumidity)
+# else:
+#     print 'Failed to get Exhaust Air reading. Try again!'
+#
+#
+#
+#
+# MBrhumidity, MBrtemp = Adafruit_DHT.read_retry(sensor, MBedroomPin)
+#
+# if MBrhumidity is not None and MBrtemp is not None:
+#     print 'Master Bedroom Air Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(MBrtemp, MBrhumidity)
+# else:
+#     print 'Failed to get Master Bedroom reading. Try again!'
---
+++ TempOneWire.py
@@ -0,0 +1,72 @@
+import os,time, datetime
+
+
+
+##############################
+## Functions
+##############################
+
+def get_filepaths(directory):
+    file_paths = [] # list to store the directories
+
+    for root,directories,files in os.walk(directory):
+            for directory in directories:
+                filepath = os.path.join(root,directory)
+                file_paths.append(filepath)
+    return file_paths
+
+
+def printReadings(full_file_paths):
+    i = 0
+    for dir in full_file_paths:
+
+        file = dir + "/w1_slave"
+        f = open(file, 'r')
+
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%d/%m/%Y %H:%M:%S')
+
+
+        reading =  f.read()
+
+        temp = getTemp(reading)
+
+        f.close()
+        values = [st,dir,temp]
+        writeTemp(values)
+	print values
+    addSpacing()
+
+def getTemp(text):
+    temp = "t="
+    index = text.find(temp)+2
+    tempc = float(text[index:])/1000.0
+    return tempc
+
+def writeTemp(values):
+
+    file = open("2016.txt",'a')
+
+    s = str(values)
+    file.write(s)
+    file.write("\n")
+
+    file.close()
+
+def addSpacing():
+	file = open("test.txt",'a')
+	file.write("\n\n\n\n\n\n")
+	print ("\n\n\n\n\n\n")
+	file.close()
+
+
+def getReadings():
+    full_file_paths = get_filepaths("/sys/bus/w1/devices")
+	full_file_paths.pop(0) #this gets rid of the non sensor directory
+    printReadings(full_file_paths)
+
+
+#
+# while(True):
+#
+#     	time.sleep(600)
