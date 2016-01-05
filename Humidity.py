85f8d88ab0a01529fa13938f50cebbd71c160432
Dean Macaulay <dean@magpies.net>
2016-01-05 11:44:17 +1100

    Added extra sensor, there are now 2

--- Humidity.py
+++ Humidity.py
@@ -4,11 +4,20 @@ import Adafruit_DHT
 
 sensor = Adafruit_DHT.DHT22
 
-pin = 18
+kitchenPin = 22
+loungePin = 24
 
-humidity, temp = Adafruit_DHT.read_retry(sensor, pin)
 
-if humidity is not None and temp is not None:
-    print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temp, humidity)
+
+khumidity, ktemp = Adafruit_DHT.read_retry(sensor, kitchenPin)
+lhumidity, ltemp = Adafruit_DHT.read_retry(sensor, loungePin)
+
+if khumidity is not None and ktemp is not None:
+    print 'Kitchen Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(ktemp, khumidity)
 else:
-    print 'Failed to get reading. Try again!'
+    print 'Failed to get Kitchen reading. Try again!'
+
+if lhumidity is not None and ltemp is not None:
+    print 'Lounge Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(ltemp, lhumidity)
+else:
+    print 'Failed to get Lounge reading. Try again!'
