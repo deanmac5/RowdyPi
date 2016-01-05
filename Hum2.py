2a2b75e482ba5856fd0f0c3cb9bf36e261adf34b
Dean Macaulay <dean@magpies.net>
2016-01-05 15:19:41 +1100

    Added new Humidity file optimised

---
+++ Hum2.py
@@ -0,0 +1,23 @@
+
+import Adafruit_DHT
+
+sensor = Adafruit_DHT.DHT22
+
+pins = {'Kitchen': 22, 'Lounge': 24, 'Supply Air': 23, 'Outside Air': 10, 'Exhaust Air': 18, 'Master Bedroom': 17}
+
+
+def readDHT(pin):
+    hum, temp = Adafruit_DHT.read_retry(sensor, pin)
+    printOutput(hum,temp)
+
+
+def printOutput(hum,temp):
+    if hum is not None and temp is not None:
+        print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temp, hum)
+    else:
+        print 'Failed to get reading. Boo hiss'
+
+#For testing
+for room,pin in pins.items():
+    print 'Room %s is pin %s' % (room,pin)
+    readDHT(pin)
