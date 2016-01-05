095a8577d8e0d3f472161087c741c14872c48540
Dean Macaulay <dean@magpies.net>
2016-01-05 16:12:29 +1100

    Starting new output and consolidation

--- MonitorAll.py
+++ MonitorAll.py
@@ -3,7 +3,10 @@
 
 import HumidityAndTemp
 import TempOneWire
+import time
 
 while(True):
     HumidityAndTemp.getHumAndTemp()
     TempOneWire.getReadings()
+    print 'Date/Time | Location | Identifier | Temperature | Humidity'
+    
