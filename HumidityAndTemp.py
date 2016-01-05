b3597164aec84a58cd75af50b26273b8cddda37e
Dean Macaulay <dean@magpies.net>
2016-01-06 09:58:44 +1100

    Added time to output

--- HumidityAndTemp.py
+++ HumidityAndTemp.py
@@ -1,18 +1,20 @@
 
 import Adafruit_DHT
-
+import time
 
 
 
 def readDHT(room,sensor,pin):
     hum, temp = Adafruit_DHT.read_retry(sensor, pin)
-    printOutput(room, hum,temp)
+    printOutput(room, hum, temp, pin)
 
 
-def printOutput(room, hum,temp):
+def printOutput(room, hum,temp, pin):
     if hum is not None and temp is not None:
-	print 'Sensor: %s' % room
-        print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(temp, hum)
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%d/%m/%Y %H:%M:%S')
+	    th = '{0:0.1f} , {1:0.1f}%'.format(temp, hum)
+        print st + room + pin + th
     else:
         print 'Failed to get reading. Boo hiss'
 
@@ -20,5 +22,4 @@ def getHumAndTemp():
     sensor = Adafruit_DHT.DHT22
     pins = {'Kitchen': 22, 'Lounge': 24, 'Supply Air': 23, 'Outside Air': 10, 'Exhaust Air': 18, 'Master Bedroom': 17}
     for room,pin in pins.items():
-        # print 'Room %s is pin %s' % (room,pin)
         readDHT(room, sensor, pin)
