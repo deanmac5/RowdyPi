a3f23125f9c736b95bde7e6ea6fdfe7005cd3519
Dean Macaulay <dean@magpies.net>
2016-01-06 11:14:24 +1100

    print all values and then write to new file

--- MonitorAll.py
+++ MonitorAll.py
@@ -1,12 +1,37 @@
 #!/usr/bin/python
 
 
-import HumidityAndTemp
-import TempOneWire
+import HumidityAndTemp,TempOneWire, time
+
+#########################
+# Functions
+#########################
+
+def writeTemp(values):
+
+    file = open("All.txt",'a')
+
+    s = str(values)
+    file.write(s)
+    file.write("\n")
+
+    file.close()
+
+def addSpacing():
+	file = open("test.txt",'a')
+	file.write("\n\n\n\n\n\n")
+	print ("\n\n\n\n\n\n")
+	file.close()
+
+
+#########################
+# Main
+#########################
 
 while(True):
     bothVals = HumidityAndTemp.getHumAndTemp()
     justTempVals = TempOneWire.getReadings()
-    print 'Date/Time | Location | Identifier | Temperature | Humidity'
-    print bothVals
-    print justTempVals
+    allValues = bothVals.append(justTempVals)
+    print allValues
+    writeTemp(allValues)
+    #time.sleep(600)
