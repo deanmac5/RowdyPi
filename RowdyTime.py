2a6b7320d14bede3284fded75f47731615cb9270
Dean Macaulay <dean@magpies.net>
2016-03-18 16:39:57 +1100

    date4 example works now

--- RowdyTime.py
+++ RowdyTime.py
@@ -15,5 +15,9 @@ from datetime import datetime
 #print date_str
 date2 = "20081004"
 time = "18:00:00"
+date3 = "20091004 16:00"
+#dt = datetime.strptime("2000-10-04 01:10:11", '%Y-%m-%d %T')
+date4 = "2015-04-24T20:09:00.052-0500"
+date5 = "1971-01-22 10:11Z"
 
-os.system('date  -s %s' % date2)
+os.system('date  -s %s' % date4)
