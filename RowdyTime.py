cade6cbe3a1e39d1726539a302836f6744fa781e
Dean <dean@magpies.net>
2016-03-19 15:15:14 +1100

    accepts user input and sets time

--- RowdyTime.py
+++ RowdyTime.py
@@ -2,22 +2,28 @@ import os
 from datetime import datetime
 
 
-#print "Enter the year"
-#year = raw_input()
+print "Enter the year"
+year = raw_input()
 
-#print "Enter the month"
-#month = raw_input()
+print "Enter the month"
+month = raw_input()
 
-#print "Enter the day"
-#day = raw_input()
+print "Enter the day"
+day = raw_input()
 
-#date_str = year+month+day
-#print date_str
-date2 = "20081004"
-time = "18:00:00"
-date3 = "20091004 16:00"
-#dt = datetime.strptime("2000-10-04 01:10:11", '%Y-%m-%d %T')
-date4 = "2015-04-24T20:09:00.052-0500"
-date5 = "1971-01-22 10:11Z"
+print "Enter the hour"
+hour = raw_input()
 
-os.system('date  -s %s' % date4)
+print "Enter the minutes"
+minutes = raw_input()
+
+
+
+date_str = year+'-'+month+'-'+day+'T'+hour+':'+minutes+':00.52-0500'
+print date_str
+
+
+date4 = "2015-04-24T20:09:00.052-0500" #this is the format we need
+
+
+os.system('date  -s %s' % date_str)
